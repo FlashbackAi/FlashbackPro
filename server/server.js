44f512e847896d07322831bc8785f48442681534
AnirudhThadem <55320091+AnirudhThadem@users.noreply.github.com>
2024-04-25 11:38:14 +0530

    Update server.js event name

--- server/server.js
+++ server/server.js
@@ -1295,7 +1295,7 @@ app.post('/downloadImage', async (req, res) => {
           const updateParamsUserEvent = {
             TableName: userEventTableName,
             Item: {
-              event_name: 'Neha_ShivaTeja_18042024',
+              event_name: 'KSL_25042024',
               user_phone_number: username,
             }
           };
