b9e717dcf93880a93b700d44f874ce763aba3962
Anirudh Thadem <anirudhthadem22@gmail.com>
2024-04-26 18:15:43 +0530

    images view as grid

---
+++ client/src/components/ImageModal.js
@@ -0,0 +1,42 @@
+const ImageModal = ({
+    clickedImg,
+    setClickedImg,
+    handelRotationRight,
+    handelRotationLeft
+  }) => {
+    const handleClick = (e) => {
+      if (e.target.classList.contains("dismiss")) {
+        setClickedImg(null);
+      }
+    };
+  
+    return (
+      <>
+        <div className="overlay dismiss" onClick={handleClick}>
+          <img src={clickedImg} alt="bigger pic" />
+          <span className="dismiss" onClick={handleClick}>
+            X
+          </span>
+          <div onClick={handelRotationLeft} className="overlay-arrows_left">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+         
+        </div>
+      </>
+    );
+  };
+  
+  export default ImageModal;
