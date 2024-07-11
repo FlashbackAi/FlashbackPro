ee396fa66a9e7a08b76c7f133ac2ddbc60d8cf31
Anirudh Thadem <anirudhthadem22@gmail.com>
2024-07-11 15:52:21 +0530

    Added Face Live Detection in Login

--- client/src/pages/Auth/Login/Login.js
+++ client/src/pages/Auth/Login/Login.js
@@ -1,6 +1,5 @@
-import React, { useRef, useState, useCallback, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-// import { AuthContext } from "../helpers/AuthContext";
 import Webcam from "react-webcam";
 import logo from "../../../media/images/logoCropped.png";
 import { toast } from "react-toastify";
@@ -8,10 +7,10 @@ import { useLocation, useParams } from "react-router-dom";
 import API_UTIL from "../../../services/AuthIntereptor";
 import CountryCodes from "../../../media/json/CountryCodes.json";
 import Select, { components } from "react-select";
-import "./login.css"
+import "./login.css";
+import * as faceapi from 'face-api.js';
 
 const CustomOption = ({ children, ...props }) => {
-  console.log(`assets/CountryFlags/${props.data.code}.png`);
   return (
     <components.Option {...props}>
       <div className="flagOption customOption" style={{ display: "flex" }}>
@@ -28,7 +27,6 @@ const CustomOption = ({ children, ...props }) => {
 };
 
 const CustomControl = ({ children, ...props }) => {
-  console.log(props);
   return (
     <components.Control {...props}>
       <div className="flagOption customControl" style={{ display: "flex" }}>
@@ -43,25 +41,23 @@ const CustomControl = ({ children, ...props }) => {
   );
 };
 
+const MODEL_URL = 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights';
+
 function Login() {
-  // const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
-  // const { setAuthState } = useContext(AuthContext);
   const { eventName } = useParams();
   const [isNewUser, setIsNewUser] = useState(false);
   const [isPhoneNumberValid, setIsPhoneNumberValid] = useState(false);
-  // const [verificationCode, setVerificationCode] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [countryCode, setCountryCode] = useState("+91");
-  // const [fullPhoneNumber, setFullPhoneNumber] = useState("");
   const [phoneNumberError, setPhoneNumberError] = useState("");
-
-  // const [faceDetected, setFaceDetected] = useState(false);
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
   const [termsAccepted, setTermsAccepted] = useState(true);
   const isToastDisp = useRef(false);
+  const [step, setStep] = useState(0); // 0: move head, 1: blink, 2: ready to capture
+  const [isCaptureEnabled, setIsCaptureEnabled] = useState(false);
 
   const videoConstraints = {
     width: 350,
@@ -73,22 +69,47 @@ function Login() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(isToastDisp.current) return;
-    toast("Register to view pictures, provide accurate mobile number so we can share you the pictures",{
+    if (isToastDisp.current) return;
+    toast("Register to view pictures, provide accurate mobile number so we can share you the pictures", {
       position: "top-center"
-    })
+    });
     isToastDisp.current = true;
-  }, [])
-  
+  }, []);
 
   const from = location.state?.from || "/home";
-  console.log(from);
+
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+    };
+
+    loadModels();
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      if (webcamRef.current && webcamRef.current.video && webcamRef.current.video.readyState === 4) {
+        const video = webcamRef.current.video;
+        if (video.videoWidth > 0 && video.videoHeight > 0) {
+          if (step === 0) {
+            await detectHeadMovement();
+          } else if (step === 1) {
+            await detectBlink();
+          }
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [step]);
 
   const handlePhoneNumberChange = (e) => {
     const value = e.target.value;
     setPhoneNumber(value);
 
-    // Regular expression for a 10-digit phone number
     const phoneRegex = /^\d{10}$/;
 
     if (!phoneRegex.test(value)) {
@@ -97,35 +118,90 @@ function Login() {
       setPhoneNumberError("");
     }
   };
-  const capture = useCallback(async () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    //const flag = await detectFaces();
-    const flag = true;
 
-    if (flag) {
-      setImgSrc(imageSrc);
-      setMessage("Please proceed Ahead");
-    } else {
-      setMessage("No faces has been detected please re-capture your picture");
+  const detectHeadMovement = async () => {
+    if (!webcamRef.current) return;
+    const video = webcamRef.current.video;
+    if (video.videoWidth === 0) return;
+
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+
+    if (detections.length > 0) {
+      const landmarks = detections[0].landmarks;
+      const nose = landmarks.getNose();
+      const noseX = nose[3].x; // The tip of the nose (center point)
+
+      // Detect significant head movement
+    
+      const videoWidth = video.videoWidth;
+      console.log(noseX, videoWidth*0.3,videoWidth*0.7)
+      if (noseX < videoWidth * 0.4 || noseX > videoWidth * 0.6) {
+        console.log('Head movement detected');
+        setStep(1);
+        setMessage("Please blink.");
+      }
     }
-  }, [webcamRef]);
+  };
+
+  const detectBlink = async () => {
+    if (!webcamRef.current) return;
+    const video = webcamRef.current.video;
+    if (video.videoWidth === 0) return;
+
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+
+    if (detections.length > 0) {
+      const landmarks = detections[0].landmarks;
+      const leftEye = landmarks.getLeftEye();
+      const rightEye = landmarks.getRightEye();
+
+      const leftEAR = calculateEAR(leftEye);
+      const rightEAR = calculateEAR(rightEye);
+
+      if (leftEAR < 0.27 && rightEAR < 0.27) {
+        setMessage("Please Capture your Selfie");
+        console.log('Blink detected');
+        setStep(2);
+       
+        setIsCaptureEnabled(true);
+      }
+    }
+  };
+
+  const calculateEAR = (eye) => {
+    const A = Math.hypot(eye[1].x - eye[5].x, eye[1].y - eye[5].y);
+    const B = Math.hypot(eye[2].x - eye[4].x, eye[2].y - eye[4].y);
+    const C = Math.hypot(eye[0].x - eye[3].x, eye[0].y - eye[3].y);
+    return (A + B) / (2.0 * C);
+  };
+
+  const capture = async () => {
+    if (isCaptureEnabled) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+      setMessage("Image captured. Please proceed.");
+    }
+  };
 
   const retake = () => {
     setImgSrc(null);
+    setStep(0);
+    setMessage("Please move your head to the left or right.");
+    setIsCaptureEnabled(false);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const fullPhoneNumber = countryCode + phoneNumber; // Combine country code and phone number
-    let userSource = "flashback"
+    const fullPhoneNumber = countryCode + phoneNumber;
+    let userSource = "flashback";
     if (from.includes("photos")) {
-      userSource = "flashback-pro"
+      userSource = "flashback-pro";
     }
     const response = await API_UTIL.post(`/createUser`, {
       username: fullPhoneNumber,
       eventName: eventName,
       userSource: userSource,
-      role:"user"
+      role: "user"
     });
     setIsPhoneNumberValid(true);
     if (response.status === 201) {
@@ -134,8 +210,6 @@ function Login() {
         position: "top-center",
       });
     } else if (response.status === 200) {
-      //setIsNewUser(true);
-      console.log(from);
       if (from.includes("photos")) {
         try {
           const urlArray = from.split("/");
@@ -156,15 +230,14 @@ function Login() {
         navigate(from);
       }
       toast(
-        `hey ${fullPhoneNumber}, you already exists. Have a great event ahead..`,
+        `hey ${fullPhoneNumber}, you already exist. Have a great event ahead..`,
         { position: "top-center" }
       );
     }
   };
 
-  const uploadPhoto = async (e) => {
+  const uploadPhoto = async () => {
     if (termsAccepted) {
-      // e.preventDefault();
       const fullPhoneNumber = countryCode + phoneNumber;
       const formData = new FormData();
       formData.append("image", imgSrc);
@@ -200,36 +273,13 @@ function Login() {
     }
   };
 
-  const handleVerification = () => {
-    navigate(from);
-    setIsNewUser(true);
-  };
-
-  useEffect(()=>{
-
-  },[])
-
   const onChangeCountryCode = (event) => {
     setCountryCode(event.label);
   };
 
-  // const resendVerification = () => {};
-
   return (
     <div className="loginBody">
-      {/* {CountryCodes.map((cnt) => {
-        return (
-          <div className="flagOption">
-            <img
-              className="iconImg"
-              src={`assets/CountryFlags/${cnt.code}.png`}
-            />
-            {cnt.code} {cnt.dial_code}
-          </div>
-        );
-      })} */}
       <div className="loginLeft">
-        {/* <p className="loginLogo">Flashback<p className="logoCaption">Create & share memories</p></p> */}
         <div className="loginLogoBox">
           <h2>
             FlashB
@@ -242,12 +292,7 @@ function Login() {
         <div className="login-form-container">
           {!isPhoneNumberValid && (
             <form onSubmit={handleSubmit} className="login-form">
-              <div
-                className={"phoneOuter " + (phoneNumberError && "error")}
-                style={{ position: "relative" }}
-              >
-                {/* <div className="countryCode"> */}
-                {/* <img src={india} /> +91 */}
+              <div className={"phoneOuter " + (phoneNumberError && "error")} style={{ position: "relative" }}>
                 <Select
                   className="countryCode"
                   classNamePrefix={"fb"}
@@ -262,61 +307,19 @@ function Login() {
                   }}
                   components={{ Option: CustomOption, Control: CustomControl }}
                 />
-                <input
-                  name="phoneNumber"
-                  required
-                  type="tel"
-                  placeholder="Phone Number"
-                  onChange={handlePhoneNumberChange}
-                />
-                {/* </div> */}
-                {/* <PhoneInput
-                className="phoneOuter"
-                placeholder="Phone Number"
-                forceDialCode={true}
-                inputClassName="inputClass"
-                  defaultCountry="in"
-                  // value={phone}
-                  // onChange={(phone) => setPhone(phone)}
-                /> */}
-
+                <input name="phoneNumber" required type="tel" placeholder="Phone Number" onChange={handlePhoneNumberChange} />
               </div>
               <div className="privacyNotice">
                 <p>By Submitting your phone number, you confirm you've read this <a href="/TermsAndConditions" target="_blank">Privacy Notice</a></p>
               </div>
               {phoneNumberError && (
-                <p
-                  style={{
-                    color: "#f66060",
-                    // backgroundColor: "#FFBABA",
-                    padding: "0.2em",
-                    fontSize: "0.8rem",
-                    borderRadius: "2px",
-                    margin: "0",
-                    textAlign: "left",
-                  }}
-                >
+                <p style={{ color: "#f66060", padding: "0.2em", fontSize: "0.8rem", borderRadius: "2px", margin: "0", textAlign: "left" }}>
                   *{phoneNumberError}
                 </p>
               )}
-              <button
-                type="submit"
-                className="submit"
-                disabled={phoneNumber.length !== 10}
-              >
-                Login
-              </button>
+              <button type="submit" className="submit" disabled={phoneNumber.length !== 10}>Login</button>
               {error && (
-                <p
-                  style={{
-                    color: "#cc0033",
-                    backgroundColor: "#FFBABA",
-                    padding: "0.2em",
-                    fontSize: "0.9rem",
-                    borderRadius: "2px",
-                    margin: "0.5em",
-                  }}
-                >
+                <p style={{ color: "#cc0033", backgroundColor: "#FFBABA", padding: "0.2em", fontSize: "0.9rem", borderRadius: "2px", margin: "0.5em" }}>
                   {error}
                 </p>
               )}
@@ -325,84 +328,44 @@ function Login() {
           {isNewUser && (
             <div className="login-form-container">
               <p className="caution">Hey, don't worry we won't see your image :)</p>
-
+              <p className={`instructions ${step === 0 ? 'rotate' : step === 1 ? 'blink' : ''}`}>
+               <span className="instructionSpan">{step === 0 ? " Please move your head left or right  to verify." : step === 1 ? " Please blink  to verify." : "Capture your Selfie"}</span>
+              </p>
               {imgSrc ? (
                 <img src={imgSrc} alt="webcam" />
               ) : (
-                <Webcam
-                  audio={false}
-                  ref={webcamRef}
-                  screenshotFormat="image/jpeg"
-                  style={{ zIndex: 0 }}
-                  videoConstraints={videoConstraints}
-                  mirrored={true}
-                  // forceScreenshotSourceSize={true}
-                />
+                <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" style={{ zIndex: 0 }} videoConstraints={videoConstraints} mirrored={true} />
               )}
               <div className="btn-container">
                 {imgSrc ? (
                   <div className="login-form-container">
                     <form className="login-form">
-                      <button type="button" onClick={uploadPhoto} className="submit submitPhoto">
-                        Submit photo
-                      </button>
-                      <button
-                        type="button"
-                        className="button takePhoto"
-                        onClick={retake}
-                      >
-                        Retake photo
-                      </button>
-                      <label
-                        style={{ display: "flex", justifyContent: "center" }}
-                      >
-                        <input
-                          name="checkbox"
-                          type="checkbox"
-                          defaultChecked
-                          required
-                          style={{ transform: "scale(1.1)" }}
-                          onChange={(e) => setTermsAccepted(e.target.checked)}
-                          onInvalid={(e) =>
-                            e.target.setCustomValidity(
-                              "Please accept the terms and conditions."
-                            )
-                          }
-                          onInput={(e) => e.target.setCustomValidity("")}
-                        />
-                        <a
-                          href="/TermsAndConditions"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="termsAndConditionButton"
-                        >
-                          Accept terms and conditions
-                        </a>
+                      <button type="button" onClick={uploadPhoto} className="submit submitPhoto">Submit photo</button>
+                      <button type="button" className="button takePhoto" onClick={retake}>Retake photo</button>
+                      <label style={{ display: "flex", justifyContent: "center" }}>
+                        <input name="checkbox" type="checkbox" defaultChecked required style={{ transform: "scale(1.1)" }} onChange={(e) => setTermsAccepted(e.target.checked)} onInvalid={(e) => e.target.setCustomValidity("Please accept the terms and conditions.")} onInput={(e) => e.target.setCustomValidity("")} />
+                        <a href="/TermsAndConditions" target="_blank" rel="noopener noreferrer" className="termsAndConditionButton">Accept terms and conditions</a>
                       </label>
                     </form>
                   </div>
                 ) : (
                   <div className="login-form-container">
-                    <form className="login-form">
-                      <button
-                        className="button takePhoto"
-                        type="button"
-                        onClick={capture}
-                      >
-                        Capture photo
-                      </button>
-                    </form>
+                    {isCaptureEnabled && (
+                      <form className="login-form">
+                        <button className="button takePhoto" type="button" onClick={capture}>Capture photo</button>
+                      </form>
+                    )}
                   </div>
                 )}
               </div>
             </div>
           )}
         </div>
-        {!isPhoneNumberValid &&(
-        <div className="contactUs">
-          <p>Contact Us: <a href="tel:+919090301234">+919090301234 | +919090401234</a></p>
-          <p>Write to Us: <text>team@flashback.inc</text></p>
-        </div>
+        {!isPhoneNumberValid && (
+          <div className="contactUs">
+            <p>Contact Us: <a href="tel:+919090301234">+919090301234 | +919090401234</a></p>
+            <p>Write to Us: <text>team@flashback.inc</text></p>
+          </div>
         )}
       </div>
     </div>
