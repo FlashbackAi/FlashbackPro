0c3a1be315304345c2718dd3ea79acc51f6924d4
vinaykumarthadem <vinaythadem@gmail.com>
2024-07-12 19:33:02 -0400

    UI Tweaks for the home page

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,15 +4,12 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="contact-container">
-        <p>&copy; 2024 Flashback Inc. All rights reserved.</p>
+        <div className="contactus">
         <p>
-          Contact Us: <a href="tel:+919090401234">+919090401234</a>
-          <a href="mailto:team@flashback.inc">,team@flashback.inc</a>
+            Contact: <a href="tel:+919090401234">+919090401234</a>
+            <a href="mailto:team@flashback.inc">,team@flashback.inc</a>
         </p>
-        <p>
-        </p>
-    </div>
+        </div>
         <div className="social-container">
           <a href="https://x.com/Flashback_Inc_" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/ffffff/m_outlined/2x/twitterx--v2.png" alt="Twitter" />
@@ -21,6 +18,13 @@ const Footer = () => {
             <img src="https://img.icons8.com/ffffff/fluent/2x/instagram-new.png" alt="Instagram" />
           </a>
         </div>
+
+      <div className="contact-container">
+        <p>&copy; 2024 Flashback Inc. All rights reserved.</p>
+
+        <p>
+        </p>
+      </div>
     </footer>
   );
 };
