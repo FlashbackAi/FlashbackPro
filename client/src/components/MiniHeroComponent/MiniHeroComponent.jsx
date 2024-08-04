f72836f02374ec2b31ed1b4113fb9dc4d8971a12
Murali Routhu <muralikrishnarouthu1818@gmail.com>
2024-08-04 18:16:46 +0530

    fix(): removes duplicate social links

--- client/src/components/MiniHeroComponent/MiniHeroComponent.jsx
+++ client/src/components/MiniHeroComponent/MiniHeroComponent.jsx
@@ -14,19 +14,7 @@ const MiniHeroComponent = () => {
     facebook: {
       url: "https://www.facebook.com/mediaaarvi",
       icon: "assets/Images/icon-facebook.svg", // Replace with your Facebook icon URL
-    },
-    instagrams: {
-      url: "https://www.instagram.com/aarvi_media/",
-      icon: "assets/Images/icon-instagram.svg", // Replace with your Instagram icon URL
-    },
-    youtubes: {
-      url: "https://www.youtube.com/@aarvimedia",
-      icon: "assets/Images/icon-youtube.svg", // Replace with your YouTube icon URL
-    },
-    facebooks: {
-      url: "https://www.facebook.com/mediaaarvi",
-      icon: "assets/Images/icon-facebook.svg", // Replace with your Facebook icon URL
-    },
+    }
     // Add more social media links and icons here
   };
   return (
