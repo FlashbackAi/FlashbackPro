0b0ac4ecd00cb7ffd62251af815ec07779986bc1
sreekanth05 <sreekanthalla16@gmail.com>
2024-08-15 19:06:19 +0530

    Replaced Flashback Inc with FLASHBACKLABS PRIVATE LIMITED FOR META VERIFICATION

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -31,8 +31,8 @@ const About = () => {
           {/*<video className="background-video" src={backgroundVideo} autoPlay loop muted />*/}
         </div>
         <div className="container header-content">
-          <h1 className='header-h1'>Flashback Inc</h1>
-          <p className='header-p'>Auto Curate & Instant Share Memories</p>
+          <h1 className='header-h1'>FLASHBACKLABS PRIVATE LIMITED</h1>
+          {/* <p className='header-p'>Auto Curate & Instant Share Memories</p> */}
           <button onClick={openModal} className="cta-button">Get Started</button>
         </div>
       </header>
