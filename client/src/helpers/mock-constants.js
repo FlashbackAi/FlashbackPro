92a2d538d760a3f340df30bce791bd7674f25c92
ROUTHU MURALI KRISHNA <46835068+muralikrishna18@users.noreply.github.com>
2024-08-19 22:30:27 +0530

    feat(event_list): adds event list page acc to figma (#34)

---
+++ client/src/helpers/mock-constants.js
@@ -0,0 +1,33 @@
+export   const mockEvents = [{
+    name: "Event Name 1",
+    imageSrc: '/assets/Images/event1-bg.png'
+  },
+  {
+    name: "Event Name 2",
+    imageSrc: '/assets/Images/event2-bg.png'
+  },
+  {
+    name: "Event Name 1",
+    imageSrc: '/assets/Images/event1-bg.png'
+  },
+  {
+    name: "Event Name 2",
+    imageSrc: '/assets/Images/event2-bg.png'
+  },
+  {
+    name: "Event Name 1",
+    imageSrc: '/assets/Images/event1-bg.png'
+  },
+  {
+    name: "Event Name 2",
+    imageSrc: '/assets/Images/event2-bg.png'
+  },
+  {
+    name: "Event Name 1",
+    imageSrc: '/assets/Images/event1-bg.png'
+  },
+  {
+    name: "Event Name 2",
+    imageSrc: '/assets/Images/event2-bg.png'
+  },
+]
