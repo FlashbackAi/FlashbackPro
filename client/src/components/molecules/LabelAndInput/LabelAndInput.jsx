9cc4db3ac7b0a7e2f1126b24b523511ab2383259
Murali Routhu <muralikrishnarouthu1818@gmail.com>
2024-08-21 00:13:26 +0530

    feat(): adds event details screen acc to figma

---
+++ client/src/components/molecules/LabelAndInput/LabelAndInput.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import InputBox from "../../atoms/InputBox/InputBox";
+import './LabelAndInput.css'
+const LabelAndInput = ({
+  label,
+  value,
+  defaultValue,
+  name,
+  isEditable,
+  type,
+  handleChange,
+}) => {
+    console.log(label)
+    console.log(value)
+  return (
+    <div className="label-text-field">
+      <span>{label}</span>
+      <InputBox
+        value={value}
+        defaultValue={defaultValue}
+        handleChange={handleChange}
+        name={name}
+        type={type}
+        isEditable={isEditable}
+      ></InputBox>
+    </div>
+  );
+};
+
+export default LabelAndInput;
