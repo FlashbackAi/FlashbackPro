a96cf1617ce316a0fe9249b086e5c775af78da81
Anirudh Thadem <anirudhthadem22@gmail.com>
2024-08-30 23:00:07 +0530

    commented rate-limit in server.js

--- server/server.js
+++ server/server.js
@@ -4934,13 +4934,13 @@ app.get("/getEventDetailsByProjectName/:projectName", async (req, res) => {
 const uploadStatus = new Map();
 
 // Rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100 // limit each IP to 100 requests per windowMs
+// });
 
-app.use(limiter);
-app.use(express.json());
+// app.use(limiter);
+// app.use(express.json());
 
 
 
