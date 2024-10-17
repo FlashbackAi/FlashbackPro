4577c1c812bac4da8d39dbcb9fcdfe8630f6a419
Anirudh Thadem <anirudhthadem22@gmail.com>
2024-10-18 00:06:08 +0530

    Scheduling missed changes

--- server/WhatsappSender.js
+++ server/WhatsappSender.js
@@ -151,6 +151,59 @@ class WhatsAppSender {
       throw error;
     }
   }
+
+  async sendEventReminder(recipientPhoneNumber, day, eventName, time, location, PortfolioLink) {
+    try {
+      const response = await axios.post(
+        this.apiUrl,
+        {
+          messaging_product: 'whatsapp',
+          to: recipientPhoneNumber,
+          type: 'template',
+          template: {
+            name: 'event_reminder',
+            language: {
+              code: 'en_US'
+            },
+            components: [
+              {
+                type: 'body',
+                parameters: [
+                  { type: 'text', text: day},
+                  { type: 'text', text: eventName },
+                  { type: 'text', text: time },
+                  { type: 'text', text: location }
+                ]
+              }
+              ,{
+                type: 'button',
+                sub_type: 'url',
+                index: 0,
+                parameters: [
+                  {
+                    type: 'text',
+                    text: PortfolioLink
+                  }
+                ]
+              }
+            ]
+          }
+        },
+        {
+          headers: {
+            'Authorization': `Bearer ${this.accessToken}`,
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+
+      console.log('Message sent successfully:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error sending Message via WhatsApp:', error.response ? error.response.data : error.message);
+      throw error;
+    }
+  }
 }
 
 module.exports = WhatsAppSender;
