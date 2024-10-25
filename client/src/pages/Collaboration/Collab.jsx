0aa5f432b09a83d26b1f778abde49267da0788e5
Anirudh Thadem <anirudhthadem22@gmail.com>
2024-10-25 17:37:23 +0530

    Collaboration page changes

--- client/src/pages/Collaboration/Collab.jsx
+++ client/src/pages/Collaboration/Collab.jsx
@@ -3,8 +3,209 @@ import { useNavigate, useParams } from 'react-router-dom';
 import API_UTIL from '../../services/AuthIntereptor';
 import { toast } from 'react-toastify';
 import AppBar from '../../components/AppBar/AppBar';
-import './Collab.css';
+import LoadingSpinner from '../../components/Loader/LoadingSpinner';
+import styled, { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  @media (max-width: 30em) { /* 480px */
+    .invite-page-container {
+      padding: 0.9375em; /* 15px */
+    }
+
+    .invite-page-container h1 {
+      font-size: 1.25em; /* 20px */
+    }
+
+    .invite-page-container p {
+      font-size: 0.875em; /* 14px */
+    }
+  }
+`;
+
+const PageWrapper = styled.div`
+  background-color: #121212;
+  min-height: 100vh;
+  color: #ffffff;
+`;
+
+const MainContent = styled.div`
+  flex: 1;
+  min-width: 0;
+`;
+
+const TopSection = styled.div`
+
+ text-align:center
+`
+
+const ContentWrapper = styled.div`
+  display: flex;
+  padding: 1rem;
+  gap: 1rem;
+  overflow-y: auto;
+  max-width: 100%;
+  margin-bottom:6em;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0.5rem;
+  }
+`;
+
+const SidePanel = styled.div`
+  background-color: #1e1e1e;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  height: fit-content;
+
+  @media (max-width: 768px) {
+    flex: 1;
+  }
+`;
+
+const EventImage = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+  overflow: hidden;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 80%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const EventTitle = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin-bottom: 1rem;
+  background: linear-gradient(90deg, #66d3ff, #9a6aff 38%, #ee75cb 71%, #fd4d77);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+ 
+`;
+const CollabPageHeader = styled.h1`
+  font-size:2em;
+  text-align:center
+`
+
+const CollabPageContainer = styled.div`
+  height:5em;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  padding: 1rem; /* 20px */
+   background-color: rgba(0, 0, 0, 0.5);
+  // box-shadow: 0 0 0.625rem rgba(0, 0, 0, 0.1); /* 10px */
+  // font-family: 'Arial', sans-serif;
+   z-index: 10; /* Ensure it's above other content */
+  // margin-left: 1em;
+  margin-bottom: 0.5em;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+     background-color: rgba(0, 0, 0, 0.5);
+      position: fixed;
+  }
+`;
+const CollabText = styled.span`
+  font-size: 2em; /* 16px */
+  color: white;
+  margin-bottom: 1em; 
+  text-align: center;
+  @media (max-width: 768px) {
+    font-size: 1em;
+  }
+
+`;
+const Label = styled.label`
+  color: #ffffff;
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  
+`;
+
+
+const Input = styled.input`
+  background-color: #ffffff;
+  border: 1px solid #3a3a3a;
+  color: #000000;
+  padding: 0.5rem;
+  border-radius: 4px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content:space-around;
+`;
+
+const InviteButton = styled.button`
+  width:30%;
+  padding: 0.625em; /* 10px */
+  font-size: 1em; /* 14px */
+  font-weight: bold;
+  color: #fff;
+  margin-top:1em;
+  background-color: #b57156;
+  border: none;
+  border-radius: 0.25em; /* 4px */
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  text-align: center;
+
+  &:hover {
+    background-color: #9f5b47;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    background-color: #d3d3d3;
+    cursor: not-allowed;
+  }
+    @media (max-width: 768px) {
+    width:8em;
+  }
+    
+`;
+const ConfirmButton = styled.button`
+  width: 25%;
+  padding: 0.625em; /* 10px */
+  font-size: 1em; /* 14px */
+  font-weight: bold;
+  color: #fff;
+  margin-top:1em;
+  background-color: #b57156;
+  border: none;
+  border-radius: 0.25em; /* 4px */
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  text-align: center;
+
+  &:hover {
+    background-color: #9f5b47;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    background-color: #d3d3d3;
+    cursor: not-allowed;
+  }
+    @media (max-width: 768px) {
+    width:8em;
+  }
+`;
 
 const Collab = () => {
   const { eventId } = useParams();
@@ -14,20 +215,25 @@ const Collab = () => {
   const [isUserNameExists, setIsUserNameExists] = useState(false);
   const navigate = useNavigate();
   const userPhoneNumber =localStorage?.userPhoneNumber;
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchEventData = async () => {
       try {
+        setIsLoading(true);
         const response = await API_UTIL.get(`/getEventDetails/${eventId}`);
         setEvent(response.data);
       } catch (error) {
         console.error('Error fetching event data:', error);
         toast.error('Failed to fetch event details.');
+      }finally{
+        setIsLoading(false);
       }
     };
 
     const fetchUserDetails = async () => {
       try {
+        setIsLoading(true);
         const response = await API_UTIL.get(`/fetchUserDetails/${userPhoneNumber}`);
         setUserDetails(response.data.data);
 
@@ -39,6 +245,8 @@ const Collab = () => {
       } catch (error) {
         console.error('Error fetching user details:', error);
         toast.error('Failed to fetch user details.');
+      }finally{
+        setIsLoading(false);
       }
     };
 
@@ -98,37 +306,51 @@ const Collab = () => {
       toast.error('Failed to update user details.');
     }
   };
+  const formatEventName = (name) => {
+    if (!name) return '';
+    let event = name.replace(/_/g, ' ');
+    return event.trim();
+  };
 
   if (!event) return <div>Loading...</div>;
 
   return (
     <>
-    <AppBar/>
-    <div className="collab-page-container">
-      <h1>Collaboration Invitation</h1>
-      <p>You have been invited to collaborate on the event: {event.event_name}</p>
-
-      {!isUserNameExists && (
-        <div className="form-group">
-          <label htmlFor="userName">User Name:</label>
-          <input
-            type="text"
-            id="userName"
-            name="userName"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder="Enter your user name"
-            required
-          />
-        </div>
-      )}
-
-      <div>
-        <button onClick={() => updateCollabStatus('Accept')}>Accept</button>
-        <button onClick={() => updateCollabStatus('Reject')}>Reject</button>
-      </div>
-    </div>
-    {/* <footer/> */}
+    {isLoading ? (
+      <LoadingSpinner/>
+    ) : (
+    <PageWrapper>
+      
+      <GlobalStyle />
+      <AppBar showCoins={true} />
+      <ContentWrapper>
+        <MainContent>
+          <CollabPageHeader>Collaboration Invitation</CollabPageHeader>
+          <EventImage>
+            <img src={event.event_image} alt="Event" />
+          </EventImage>
+        </MainContent>
+      </ContentWrapper>
+      <CollabPageContainer>
+            <CollabText>{formatEventName(event?.event_name)}</CollabText>
+            {!isUserNameExists &&(
+              <>
+            <Label>Enter your user name : </Label>
+                <Input
+                  type="text"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+              </> 
+              )}
+            <ButtonContainer>
+              <InviteButton onClick={() => updateCollabStatus('Accept')}>Accept</InviteButton>
+              <InviteButton onClick={() => updateCollabStatus('Reject')}>Reject</InviteButton>
+            </ButtonContainer>
+             
+      </CollabPageContainer>
+    </PageWrapper>
+    )}
     </>
   );
 };
